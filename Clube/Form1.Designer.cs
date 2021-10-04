01061c0c77612bdd2ef60a35423090c7bc2d27e1
Evandro Orlandini <84678879+EvandroOrlandini@users.noreply.github.com>
2021-10-03 23:39:16 -0300

    Add files via upload

---
+++ Clube/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿
+namespace clube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(913, 469);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTelefone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMensalidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnAtivo;
+    }
+}
+
---
+++ Clube/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace clube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+          InitializeComponent();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Server.Database = "clube";
+            conexaoBD.UserID ="root";
+            conexaoBD.Password = "";
+            return conexaoBD;
+        }
+
+        private void atualizarGrid()
+        { 
+           MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+           MySqlConnection realizaConexaoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexaoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexaoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM membro";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgClube.Rows.Clear(); 
+                //ou membro
+
+                while (reader.Read());
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgClube.Rows[0].Clone();
+                     
+                    row.Cells[0].value = reader.GetInt32(0); //ID
+                    row.Cells[1].value = reader.GetString(1); //NOME
+                    row.Cells[2].value = reader.GetString(2); //TELEFONE
+                    row.Cells[3].value = reader.GetString(3); //EMAIL
+                    row.Cells[4].value = reader.GetString(4); // AULA
+                    row.Cells[5].value = reader.GetString(5); //MENSALIDADE
+                    row.CellsCells[6].value = reader.GetString(6); //ATIVO
+                    dgClube.Rows.Add(row);
+                }
+                realizaConexaoBD.Close();
+            }
+
+            catch (Exception ex)
+            { 
+                MessageBox.Show("Can not open connection! ");
+            Console.WriteLine(ex Message);
+
+            }
+                  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizarGrid();
+        }
+    }
+
+   
